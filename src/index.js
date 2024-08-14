00b9e47030822a17c12641575169a351ca5cc58d
Lance Borden <103229880+BanceDev@users.noreply.github.com>
2024-08-14 08:59:41 -0400

    basic page functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 export default function (Alpine) {
-  Alpine.directive(
-    '[name]',
-    (el, { value, modifiers, expression }, { Alpine, effect, cleanup }) => {}
-  )
+  Alpine.page = (name, component) => {
+    // Create an alpine data for the local page data
+    Alpine.data(name, () => {
+      const obj = component();
 
-  Alpine.magic('[name]', (el, { Alpine }) => {})
+      // require the page function
+      if (typeof obj.page !== 'function') {
+        throw new Error('Alpine.page components must contain a "page" function');
+      }
+
+      // intercept and modify the init function
+      const originalInit = obj.init || function() {};
+      obj.init = function() {
+        const htmlContent = obj.page();
+        this.$el.innerHTML = htmlContent;
+        originalInit.call(this);
+      }
+
+      return obj;
+    });
+  };
+
+  Alpine.directive('page', (el, { expression }) => {
+    el.setAttribute('x-data', expression);
+    el.setAttribute('x-show', `$page === ${expression}`);
+    el.setAttribute('x-effect', 'page()');
+  });
+
+
 }
