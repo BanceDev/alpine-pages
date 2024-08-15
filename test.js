4f7dfdaffd3adc0e28d334ccf8b7e82d0666474b
BanceDev <lanceaborden@protonmail.com>
2024-08-15 00:04:07 -0400

    removed the test files

--- test.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("alpine:init", () => {
-    Alpine.data('home', () => ({
-        message: 'hello',
-        page() {
-            return `<h2>${this.message}</h2>`;
-        }
-    }));
-});
